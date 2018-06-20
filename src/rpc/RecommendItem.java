9429f6822342b63f723bf1588ffbcf8f9921fca8
Jingyang Zhang <jingyangzhang@172-16-41-72.dynapool.nyu.edu>
2018-06-20 14:58:31 -0400

    0620

---
+++ src/rpc/RecommendItem.java
@@ -0,0 +1,70 @@
+package rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Servlet implementation class RecommendItem
+ */
+@WebServlet("/recommendation") // url mapping
+public class RecommendItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RecommendItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*1 return a list of fake data*/
+		response.setContentType("application/json");
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		PrintWriter out = response.getWriter();
+
+		JSONArray array = new JSONArray();
+		try {
+			JSONObject jo1 = new JSONObject();
+			JSONObject jo2 = new JSONObject();
+			
+			jo1.put("name", "abcd");
+			jo1.put("address", "san francisco");
+			jo1.put("time", "01/01/2017");
+			
+			jo2.put("name", "1234");
+			jo2.put("address", "san jose");
+			jo2.put("time", "01/02/2017");
+			
+			array.put(jo1);
+			array.put(jo2);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		out.print(array);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
